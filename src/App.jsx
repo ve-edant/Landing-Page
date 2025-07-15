f8435da6b8bc2be4d3c1b064a9cb31617f0fd5a7
Dattatray Sirsat <dattatraysirsat@192.168.1.13>
2025-07-15 16:35:31 +0530

    Routing update.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import Clients from "./pages/Clients";
 import About from "./pages/About";
 import Knowledge from "./pages/Knowledge";
 import Contact from "./pages/Contact";
+import AboutUs from "./Subpages/AboutUs";
+import Design from "./Subpages/Design";
+import Technology from "./Subpages/Technology";
+import { Career } from "./Subpages/Career";
+import Team from "./Subpages/Team";
+import Marketing from "./Subpages/Marketing";
 
 function App() {
   return (
@@ -19,6 +25,12 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/knowledge" element={<Knowledge />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/design" element={<Design />} />
+          <Route path="/marketing" element={<Marketing />} />
+          <Route path="/technology" element={<Technology />} />
         </Routes>
       </MainLayout>
     </Router>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <section>We,at Daflitech, apply neuromarketing and behavioural science to design,digitize and grow your bussiness.</section>
+      <section> At the intersection of art,technology and bussiness, we create design solution geared towards business sucess.
+      Since 2012, we have been global leaders in design, technology.Our services range experience designing,identity development,product
+      strategy,branding,user experience design,user interface desgin,technology development to digitial marketing and SE0.
+      </section>
+    </div>
   )
 }
 
